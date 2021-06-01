e62b4796507eee20d271f5bbee6cd7a1b03f40c1
kerasus <aliesmaeeli20@gmail.com>
2021-06-01 11:31:25 +0430

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,131 +1,28 @@
 <template>
-  <div id="app" >
-  <div class="container">
-    <div class="row  main-content">
-      <div v-for="(productItem, index) in productItems" :key="index" class="col-md-3 col-sm-6 col-12">
-
-
-          <productItem :product="productItem"></productItem>
-
-      </div>
-    </div>
-  </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import './assets/style/font.css'
-import './assets/style/style1.css'
-import './assets/style/style2.css'
-import productItem from './components/productItem.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  data(){
-    return {
-      productItems:[
-        {
-          image: 'https://cdn.alaatv.com/upload/images/product/01_20210510085445.jpg?w=331&h=331',
-          title: 'همایش آرش جمع بندی علوم و فنون ادبی کنکور',
-          author: 'محمد صادق ثابتی',
-          duration: '۲۰ ساعت',
-          price: {
-            base: 12000000,
-            final: 11500000,
-          },
-        },
-        {
-          image: 'https://cdn.alaatv.com/upload/images/product/02_20210510085432.jpg?w=331&h=331',
-          title: 'همایش آرش جمع بندی علوم و فنون ادبی کنکور',
-          author: 'محمد صادق ثابتی',
-          duration: '۲۰ ساعت',
-          price: {
-            base: 12000000,
-            final: 11500000,
-          },
-        },
-        {
-          image: 'https://cdn.alaatv.com/upload/images/product/07_20210510085547.jpg?w=331&h=331',
-          title: 'همایش آرش جمع بندی علوم و فنون ادبی کنکور',
-          author: 'محمد صادق ثابتی',
-          duration: '۲۰ ساعت',
-          price: {
-            base: 12000000,
-            final: 11500000,
-          },
-        },
-        {
-          image: 'https://cdn.alaatv.com/upload/images/product/08_20210510085456.jpg?w=331&h=331',
-          title: 'همایش آرش جمع بندی علوم و فنون ادبی کنکور',
-          author: 'محمد صادق ثابتی',
-          duration: '۲۰ ساعت',
-          price: {
-            base: 12000000,
-            final: 11500000,
-          },
-        },
-        {
-          image: 'https://cdn.alaatv.com/upload/images/product/12_20210510085405.jpg?w=331&h=331',
-          title: 'همایش آرش جمع بندی علوم و فنون ادبی کنکور',
-          author: 'محمد صادق ثابتی',
-          duration: '۲۰ ساعت',
-          price: {
-            base: 12000000,
-            final: 11500000,
-          },
-        },
-        {
-          image: 'https://cdn.alaatv.com/upload/images/product/11_20210510085516.jpg?w=331&h=331',
-          title: 'همایش آرش جمع بندی علوم و فنون ادبی کنکور',
-          author: 'محمد صادق ثابتی',
-          duration: '۲۰ ساعت',
-          price: {
-            base: 12000000,
-           final: 11500000,
-          },
-        },
-        {
-          image: 'https://cdn.alaatv.com/upload/images/product/06_20210510085352.jpg?w=331&h=331',
-          title: 'همایش آرش جمع بندی علوم و فنون ادبی کنکور',
-          author: 'محمد صادق ثابتی',
-          duration: '۲۰ ساعت',
-          price: {
-            base: 12000000,
-            final: 11500000,
-          },
-        },
-        {
-          image: 'https://cdn.alaatv.com/upload/images/product/02_20210510085432.jpg',
-          title: 'همایش آرش جمع بندی علوم و فنون ادبی کنکور',
-          author: 'محمد صادق ثابتی',
-          duration: '۲۰ ساعت',
-          price: {
-            base: 12000000,
-            final: 11500000,
-          },
-        }
-      ]
-    }
-  },
   name: 'App',
   components: {
-    productItem
+    HelloWorld
   }
 }
 </script>
 
-<style scoped>
-.main-content{
-  margin-top: 100px;
-
-
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
-
-#app{
-  width: 100%;
-  height: 100%;
-}
-
-
-
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VuePersianDigit from 'vue-persian-digit'
-
-Vue.use(VuePersianDigit);
 
 Vue.config.productionTip = false
 
