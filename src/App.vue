540b2ae0d9fa14a783f9d012f004260dd4f2e870
unknown <mitra_khorram@yahoo.com>
2021-06-01 05:17:59 -0700

    box content  created

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,34 @@
-<template>
-  <div id="app">
-
+<template xmlns="http://www.w3.org/1999/html">
+  <div id="app" >
   <div class="container">
-    <div class="row">
+    <div class="row  main-content">
       <div class="col-md-3 col-sm-6 col-12">
-        <div class="box"></div>
+        <div class="box">
+          <productItem></productItem>
+        </div>
       </div>
       <div class="col-md-3 col-sm-6 col-12">
-        <div class="box"></div>
+        <div class="box">
+          <productItem></productItem>
+        </div>
       </div>
       <div class="col-md-3 col-sm-6 col-12">
-        <div class="box"></div>
+        <div class="box">
+          <productItem></productItem>
+        </div>
       </div>
       <div class="col-md-3 col-sm-6 col-12">
-        <div class="box"></div>
+        <div class="box">
+          <productItem></productItem>
+        </div>
       </div>
       <div class="col-md-3 col-sm-6 col-12">
-        <div class="box"></div>
-      </div>
-      <div class="col-md-3 col-sm-6 col-12">
-        <div class="box"></div>
-      </div>
-      <div class="col-md-3 col-sm-6 col-12">
-        <div class="box"></div>
-      </div>
-      <div class="col-md-3 col-sm-6 col-12">
-        <div class="box"></div>
+        <div class="box">
+          <productItem></productItem>
+        </div>
       </div>
     </div>
   </div>
-    <productItem></productItem>
   </div>
 </template>
 
@@ -46,9 +45,21 @@ export default {
 </script>
 
 <style>
+.main-content{
+  margin-top: 100px;
+}
+#app{
+  width: 100%;
+  height: 100%;
+ background-color: #f0ba51 ;
+}
+
 .box {
   width: 100%;
-  height: 100px;
+  height: 375px;
+  border-radius: 20px;
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16);
+  background-color: #ffffff;
   border: solid 1px red;
 }
 
