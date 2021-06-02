0ea9b1c275e2a80cd3ed5f8e32648f6f974c183b
mitra <mitra_khorram@yahoo.com>
2021-06-02 21:08:38 +0430

    1

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template xmlns="http://www.w3.org/1999/html">
+  <div id="app" >
+  <div class="container">
+    <div class="row  main-content">
+      <div class="col-md-3 col-sm-6 col-12">
+        <div class="box">
+          <productItem></productItem>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-12">
+        <div class="box">
+          <productItem></productItem>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-12">
+        <div class="box">
+          <productItem></productItem>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-12">
+        <div class="box">
+          <productItem></productItem>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-12">
+        <div class="box">
+          <productItem></productItem>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+
+import productItem from './components/productItem.vue'
+
+export default {
+  name: 'App',
+  components: {
+    productItem
+  }
+}
+</script>
+
+<style>
+.main-content{
+  margin-top: 100px;
+}
+#app{
+  width: 100%;
+  height: 100%;
+ background-color: #f0ba51 ;
+}
+
+.box {
+  width: 100%;
+  height: 375px;
+  border-radius: 20px;
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16);
+  background-color: #ffffff;
+  border: solid 1px red;
+}
+
+</style>
---
+++ src/components/productItem.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container">
+    <div class="img-box">
+      <img src="@/assets/images/img1.jpg" class="img" alt="img1">
+      img</div>
+    <div class="content"
+    >
+      content
+      <div class="main-title">
+         همایش آرش جمع بندی علوم و فنون
+ادبی کنکور
+      </div>
+      <div class="time-author-box">
+        <div class="author">
+            <div class="item">A</div>
+       <div>محمد صادقی</div>
+        
+        </div>
+        <div class="time">
+           <div class="item">B</div>
+          <div>20 ساعت</div>
+         
+        </div>
+      </div>
+      <div>
+        <div class="priceBox">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="discount"></div>
+        <div class="icon"></div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.img{
+  width: 100%;
+  height: 100%;
+
+}
+.img-box{
+
+}
+.time-author-box{
+ display: flex;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+}
+.author{
+   display: flex;
+  flex-wrap: nowrap;
+
+}
+.time{
+     display: flex;
+  flex-wrap: nowrap;
+
+}
+.item{
+  padding-left:6px ;
+}
+.container{
+  padding: 15px;
+}
+@media only screen and (max-width:375px){
+
+  .img{
+    background-color: blue;
+
+  }
+}
+</style>
